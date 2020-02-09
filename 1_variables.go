6cc2a0ab9257c1877b91b1967907c0dc2000cbe8
irwanian <irwan.ramadhan@agenkan.co.id>
2020-02-09 10:43:54 +0700

    fizzbuzzing

--- 1_variables.go
+++ 1_variables.go
@@ -9,16 +9,29 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello")
+	var x = "I'm Hungry"
+	fmt.Println(x)
 	foo()
 	fmt.Println("Go back to main")
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 7; i++ {
 		if i%3 == 0 {
 			fmt.Println(i)
 		}
 	}
+	fmt.Println("Bella Ciao...")
 }
 
 func foo() {
 	fmt.Println("Foo is Executed")
+	for i := 1; i < 31; i++ {
+		if i%3 == 0 && i%5 != 0 {
+			fmt.Println("Fizz")
+		} else if i%5 == 0 && i%3 != 0 {
+			fmt.Println("Buzz")
+		} else if i%3 == 0 && i%5 == 0 {
+			fmt.Println("FizzBuzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
 }
